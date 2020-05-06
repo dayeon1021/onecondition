5b2cde4d1e2bff2e10353ea08916b54f467a0efd
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-06 21:43:37 +0900

    2020.05.06 실습보고서 문제 4번

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -9,6 +9,14 @@ namespace onecondition
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x > 10 && x < 20)
+            {
+                Console.WriteLine("조건에 맞습니다.");
+            }
+        }
     }
 }
